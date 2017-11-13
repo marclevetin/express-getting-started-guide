5a4ca9615e125a38d390a8cb97868c9ea7b6c4e5
marclevetin <marc@derbyhat.org>
2017-11-13 01:30:09 -0500

    birds.  learning about routes in external files

--- myapp/app.js
+++ myapp/app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+// next line is the bird example.
+var birds = require('./routes/birds')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -24,6 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+// bird example
+// remember for next time, all routes need to appear _before_ the 404 error handler.
+// Rookie mistake, dude.  Rookie mistake.  :)
+app.use('/birds', birds)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +50,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
